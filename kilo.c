01cf6bcf83ddfe2c9e218beae70fe32f4c45ebd3
Garvit <garvitbibra@outlook.com>
2024-01-15 19:48:47 -0600

    Canonical mode diabled, terminal displays keypress

--- kilo.c
+++ kilo.c
@@ -1,6 +1,8 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios orig_termios;
 
@@ -11,16 +13,26 @@ void disableRawMode(){
 void enablerawMode(){
 
     tcgetattr(STDIN_FILENO, &orig_termios);
-    //atexit(disableRawMode);
+    atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON); //reading input byte-by-byte instead of line-by-line
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(){
+
+    enablerawMode();
+
     char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if(iscntrl(c)){
+            printf("%d\n", c);
+        }else{
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
     return 1;
 }
